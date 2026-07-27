5a3e449e2598fd8a5c7b9d4c8c3c5fd989b6ead3
leticia.youssef <leticiayoussef@movida.com.br>
2026-07-27 14:26:06 -0300

    analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { inject } from '@vercel/analytics';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+inject({ mode: isDevMode() ? 'development' : 'production' });
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
