dbdbd169bc21b7a1444a52aa9d00b286e2017428
leticia.youssef <leticiayoussef@movida.com.br>
2026-07-28 10:57:57 -0300

    ajustes dashboard

--- src/app/pages/admin/dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin/dashboard/admin-dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { AlbumService, UserService } from '../../../core/services/album.service';
 import { map } from 'rxjs';
+import { AlbumService, UserService } from '../../../core/services/album.service';
+import { TestimonialService } from '../../../core/services/testimonial.service';
+import { GalleryService } from '../../../core/services/gallery.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -14,8 +16,12 @@ import { map } from 'rxjs';
 export class AdminDashboardComponent {
   private readonly albums = inject(AlbumService);
   private readonly users = inject(UserService);
+  private readonly testimonials = inject(TestimonialService);
+  private readonly gallery = inject(GalleryService);
 
   readonly clientCount$ = this.users.getClients().pipe(map((list) => list.length));
   readonly albumCount$ = this.albums.getAll().pipe(map((list) => list.length));
+  readonly testimonialCount$ = this.testimonials.getAll().pipe(map((list) => list.length));
+  readonly galleryCount$ = this.gallery.getAll().pipe(map((list) => list.length));
   readonly recentAlbums$ = this.albums.getAll().pipe(map((list) => list.slice(0, 5)));
 }
